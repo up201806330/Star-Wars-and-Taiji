9619f007876c49a106c38c5df01d7e9e68e9f703
Rafael Ribeiro <up201806330@fe.up.pt>
2020-10-06 11:21:31 +0100

    Triangle primitive

--- TP1/MyPrimitive.js
+++ TP1/MyPrimitive.js
@@ -6,16 +6,33 @@ class MyPrimitive {
         this.graph = graph;
         this.aPrimitive;
         
+        console.log("Leaf is: " + type);
+        
         if (type == "rectangle") {
-            console.log("Leaf is: " + type);
 
             let x1 = this.graph.reader.getFloat(element, "x1", true);
-            let x2 = this.graph.reader.getFloat(element, "x2", true);
             let y1 = this.graph.reader.getFloat(element, "y1", true);
+            let x2 = this.graph.reader.getFloat(element, "x2", true);
             let y2 = this.graph.reader.getFloat(element, "y2", true);
 
             this.aPrimitive = new MyRectangle(this.graph.scene, x1, y1, x2, y2);
             // o problema é que tinha posto x1,x2,y1,y2 em vez de x1,y1,x2,y2
+        }
+
+        else if (type == "triangle"){
+            let x1 = this.graph.reader.getFloat(element, "x1", true);
+            let y1 = this.graph.reader.getFloat(element, "y1", true);
+            let x2 = this.graph.reader.getFloat(element, "x2", true);
+            let y2 = this.graph.reader.getFloat(element, "y2", true);
+            let x3 = this.graph.reader.getFloat(element, "x3", true);
+            let y3 = this.graph.reader.getFloat(element, "y3", true);
+
+            this.aPrimitive = new MyTriangle(this.graph.scene, x1, y1, x2, y2, x3, y3);
+            
+        }
+
+        else {
+            console.log("Not Implemented!");
         }
 
     }
--- TP1/main.js
+++ TP1/main.js
@@ -11,7 +11,7 @@ function getUrlVars() {
     return vars;
 }	 
 //Include additional files here
-serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js', 'MyPrimitive.js', 'MyNode.js',
+serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js', 'MyPrimitive.js', 'MyNode.js', 'MyTriangle.js',
 
 main=function()
 {
