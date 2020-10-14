5af769bc6f99611bb92963967115221d9bc6c82e
Rafael Ribeiro <up201806330@fe.up.pt>
2020-10-14 18:21:05 +0100

    Finished texturing X wing

--- TP1/Primitives/MyTriangle.js
+++ TP1/Primitives/MyTriangle.js
@@ -86,6 +86,9 @@ class MyTriangle extends CGFobject {
 		let distA = Math.sqrt(Math.pow(this.v2[0] - this.v1[0], 2) + Math.pow(this.v2[1] - this.v1[1], 2) + Math.pow(this.v2[2] - this.v1[2], 2));
 		let distB = Math.sqrt(Math.pow(this.v3[0] - this.v2[0], 2) + Math.pow(this.v3[1] - this.v2[1], 2) + Math.pow(this.v3[2] - this.v2[2], 2));
 		let distC = Math.sqrt(Math.pow(this.v1[0] - this.v3[0], 2) + Math.pow(this.v1[1] - this.v3[1], 2) + Math.pow(this.v1[2] - this.v3[2], 2));
+		let temp1 = Math.pow(distA, 2);
+		let temp2 = Math.pow(distB, 2);
+		let temp3 = (Math.pow(distA, 2) - Math.pow(distB, 2) + Math.pow(distC, 2));
 		let cosAlpha = (Math.pow(distA, 2) - Math.pow(distB, 2) + Math.pow(distC, 2)) / (2 * distA * distC);
 		let sinAlpha = Math.sqrt(1 - Math.pow(cosAlpha, 2));
 		let alpha = Math.acos(cosAlpha);
@@ -93,7 +96,7 @@ class MyTriangle extends CGFobject {
 		let T3y = distC * sinAlpha / aft;
 		this.texCoords = [
 			0, 0,
-			distA / afs, 0,
+			distA / afs/2, 0,
 			T3x, T3y
 		];
 
