868d98c7eddf4c96ec5c87f64529693b466e6f45
Joao Sousa <joaosousa.pt@gmail.com>
2020-11-17 12:21:48 +0000

    Progress on Plane primitive (pretty much done?)

---
+++ TP2/Primitives/Plane.js
@@ -0,0 +1,32 @@
+class Plane extends CGFobject {
+    constructor(scene, uDivisions, vDivisions) {
+        super(scene);
+        this.uDivisions = uDivisions;
+        this.vDivisions = vDivisions;
+
+        this.createSurface();
+    }
+
+    createSurface() {
+        this.nurbsSurface = new CGFnurbsSurface(
+            1, // degree on U: 2 control vertexes U
+            1, // degree on V: 2 control vertexes on V
+           [	// U = 0
+               [ // V = 0..1;
+                    [-0.5, -0.5, 0.0, 1 ],
+                    [-0.5,  0.5, 0.0, 1 ]
+                   
+               ],
+               // U = 1
+               [ // V = 0..1
+                    [ 0.5, -0.5, 0.0, 1 ],
+                    [ 0.5,  0.5, 0.0, 1 ]							 
+               ]
+           ]
+        );
+
+        this.nurbsObject = new CGFnurbsObject(this.scene, this.uDivisions, this.vDivisions, this.nurbsSurface);
+    }
+
+
+}
