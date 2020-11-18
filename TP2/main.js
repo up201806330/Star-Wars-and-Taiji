0175b5e11e6b025e68c5c01465e222a27122f480
Rafael Ribeiro <up201806330@fe.up.pt>
2020-11-18 03:35:49 +0000

    Added new stuff to scene

--- TP2/main.js
+++ TP2/main.js
@@ -30,7 +30,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
-    var filename=getUrlVars()['file'] || "test.xml";
+    var filename=getUrlVars()['file'] || "LAIG_TP2_XML_T5_G01_v03.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
