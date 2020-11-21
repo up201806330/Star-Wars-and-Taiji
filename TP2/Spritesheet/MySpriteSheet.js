d4cfb3e82b71ec814e649f78876a9fd28dc2a778
Rafael Ribeiro <up201806330@fe.up.pt>
2020-11-21 17:35:56 +0000

    Update MySpriteSheet.js

--- TP2/Spritesheet/MySpriteSheet.js
+++ TP2/Spritesheet/MySpriteSheet.js
@@ -6,16 +6,16 @@ class MySpriteSheet{
         this.sizeN = sizeN;
 
         this.shader = new CGFshader(scene.gl, "./Spritesheet/sprite.vert", "./Spritesheet/sprite.frag");
+        this.shader.setUniformsValues({u_texture: this.texture});
+        let mn = vec2.fromValues(this.sizeM, this.sizeN);
+        this.shader.setUniformsValues({size: mn});
     }
 
     activateCellMN(m, n){
         //console.log(m + " ; " + n);
         //console.log(this.texture);
         let coords = vec2.fromValues(m, n);
-        let mn = vec2.fromValues(this.sizeM, this.sizeN);
         this.shader.setUniformsValues({texCoords: coords});
-        this.shader.setUniformsValues({u_texture: this.texture});
-        this.shader.setUniformsValues({size: mn});
     }
 
     activateCellP(p){
