7187bed6167c82221dc002a8788b1006a684db1c
Rafael Ribeiro <up201806330@fe.up.pt>
2020-12-27 22:43:25 +0000

    Communication with prolog outlined

--- TP3/game/Client.js
+++ TP3/game/Client.js
@@ -1,27 +1,28 @@
-function getPrologRequest(requestString, onSuccess, onError, port) {
-    var requestPort = port || 8081;
-    var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:" + requestPort + "/" + requestString, true);
+class Client{
 
-    request.onload = onSuccess || function (data) { console.log("Request successful. Reply: " + data.target.response); };
-    request.onerror = onError || function () { console.log("Error waiting for response"); };
+    getPrologRequest(requestString, onSuccess, onError, port) {
+        var requestPort = port || 8081;
+        var request = new XMLHttpRequest();
+        request.open("GET", "http://localhost:" + requestPort + "/" + requestString, true);
+    
+        request.onload = onSuccess || function (data) { console.log("Request successful. Reply: " + data.target.response); };
+        request.onerror = onError || function () { console.log("Error waiting for response"); };
+    
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        request.send();
+    }
+    
+    makeRequest(requestString) {
+        // Make Request
+        getPrologRequest(requestString, handleReply /*, handleError default aqui */);
+    }
+    
+    // Handle the Reply
+    handleReply(data) {
+        // document.querySelector("#query_result").innerHTML = data.target.response;
+        console.log(data.target.response);
+    
+        return data.target.response;
+    }
 
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-    request.send();
-}
-
-function makeRequest() {
-    // Get Parameter Values
-    var requestString = document.querySelector("#query_field").value;
-  
-    // Make Request
-    getPrologRequest(requestString, handleReply);
-}
-
-// Handle the Reply
-function handleReply(data) {
-    // document.querySelector("#query_result").innerHTML = data.target.response;
-    console.log(data.target.response);
-
-    return data.target.response;
 }
