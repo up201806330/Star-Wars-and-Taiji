d12a885899850160c40d9512516815e0f70def46
Rafael Ribeiro <up201806330@fe.up.pt>
2020-12-28 22:55:05 +0000

    and i oop

--- TP3/gameBoard/MyPiece.js
+++ TP3/gameBoard/MyPiece.js
@@ -13,19 +13,24 @@ class MyPiece {
 
         console.log([rowW, colW, rowB, colB]);
 
+        this.initPieceMaterials();
+
+        this.blackPart = new MyUnitCubeQuad(scene, this.blackMaterial, this.blackMaterial);
+        this.whitePart = new MyUnitCubeQuad(scene, this.whiteMaterial, this.whiteMaterial);
+    }
+
+    initPieceMaterials(){
         this.blackMaterial = new CGFappearance(scene);
         this.blackMaterial.setSpecular(1, 1, 1, 1);
         this.blackMaterial.setShininess(120);
         this.blackMaterial.setAmbient(0, 0, 0, 1);
         this.blackMaterial.setDiffuse(0.2, 0.2, 0.2, 1);
+        
         this.whiteMaterial = new CGFappearance(scene);
         this.whiteMaterial.setSpecular(1, 1, 1, 1);
         this.whiteMaterial.setShininess(120);
         this.whiteMaterial.setAmbient(0.8, 0.8, 0.8, 1);
         this.whiteMaterial.setDiffuse(1, 1, 1, 1);
-
-        this.blackPart = new MyUnitCubeQuad(scene, this.blackMaterial, this.blackMaterial);
-        this.whitePart = new MyUnitCubeQuad(scene, this.whiteMaterial, this.whiteMaterial);
     }
 
     startAnimation(scene, startTime){
